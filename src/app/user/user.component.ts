486a0b90b3d2ee61783cee949aefa4c85d036501
Borna <bornamashaye@gmail.com>
2025-04-20 11:19:40 +0330

    getting user data using input component and displayig multiple users

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,5 @@
-import { Component, signal, computed } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-user';
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
+
 @Component({
   selector: 'app-user',
   imports: [],
@@ -8,20 +7,10 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // using state managment mechanism
-  // selectedUser = DUMMY_USERS[randomIndex];
-  // get imagePath() {
-  //   return `assets/users/${this.selectedUser.avatar}`;
-  // }
-  // onSelectUser() {
-  //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-  // this.selectedUser = DUMMY_USERS[randomIndex];
-  // }
-  // using signal method
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => `assets/users/${this.selectedUser().avatar}`);
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  @Input({required:true}) avatar!: string;
+  @Input({required:true}) name!: string;
+  get imagePath() {
+    return `assets/users/${this.avatar}`
   }
+  onSelectUser() {}
 }
